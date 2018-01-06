78010f49336c75a54f6c68dd2eb4ef7c5a8003af
george <tzafge96@gmail.com>
2018-01-06 04:05:43 +0200

    button functionality

--- app/src/main/java/com/example/erie/tictactoe/StartActivity.java
+++ app/src/main/java/com/example/erie/tictactoe/StartActivity.java
@@ -1,6 +1,8 @@
 package com.example.erie.tictactoe;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -40,5 +42,28 @@ public class StartActivity extends AppCompatActivity {
         });
 
 
+
     }
-}
+    public void clickExit(View v){
+        finish();}
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle("Really Exit?")
+                .setMessage("Are you sure you want to exit?")
+                .setNegativeButton(android.R.string.no, null)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+
+
+
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        StartActivity.super.onBackPressed();
+                    }
+                }).create().show();
+    }
+
+    }
+
+
