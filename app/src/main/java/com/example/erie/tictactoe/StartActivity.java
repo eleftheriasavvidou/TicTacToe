0402735ad3483a902caf1adb1299e5e6bff43227
eleftheriasavvidou <erie96@gmail.com>
2018-01-06 03:28:30 +0200

    help button play button

--- app/src/main/java/com/example/erie/tictactoe/StartActivity.java
+++ app/src/main/java/com/example/erie/tictactoe/StartActivity.java
@@ -1,13 +1,44 @@
 package com.example.erie.tictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    Button help;
+    Button play;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        help = (Button)findViewById(R.id.button2);
+
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent helpint = new Intent(StartActivity.this,HelpActivity.class);
+
+                startActivity(helpint);
+            }
+        });
+
+
+        play = (Button)findViewById(R.id.button);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent playint = new Intent(StartActivity.this,MainActivity.class);
+
+                startActivity(playint);
+            }
+        });
+
+
     }
 }
